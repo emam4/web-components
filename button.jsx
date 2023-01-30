d4d3dff29940ae4df9b4f65067394c79bb8d78f3
emam4 <noureldin.ahmedemam@twerlo.com>
2023-01-30 14:11:02 +0200

    >>

--- button.jsx
+++ button.jsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './button.css';
+import React from 'react';
 
 function SpecialButton() {
-  const [post, setPost] = useState({})
-  const [postID, setPostID] = useState(1)
-  const [IDFromButton, setIDButton] = useState(1)
-
-  const clickHandler = () => {
-    setIDButton(postID)
-  }
-  useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${IDFromButton}`).then((res) => {
-    setPost(res.data)    
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  },[IDFromButton])
   return (
     <div>
-      <p>{post.title}</p>
-      <input placeholder='Type post id' onChange={e=> setPostID(e.target.value)} />
-      <button type='button' onClick={clickHandler}>set post ID</button>
+      <button type='button' >set post ID</button>
     </div>
   )
 }
